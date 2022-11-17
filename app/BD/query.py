bb22753f0c2080e88c29386f602bf8922b510215
CarlosKC26 <cgalvisn@unal.edu.co>
2022-11-17 03:43:02 -0500

    Querys update

--- app/BD/query.py
+++ app/BD/query.py
@@ -1,30 +1,40 @@
-def register(connection, email, nombre,contrasena,direccion,tarjeta):
+def registerQuery(connection, email, nombre,contrasena,direccion,tarjeta):
     mycursor = connection.cursor()
     mycursor.execute("SELECT `E_MAIL` FROM `loveinbox`.`usuario`")
 
     myresult = mycursor.fetchall()
 
-    idUsuario=myresult
-
-    for x in myresult[0]:
-        if(email==x):
+    for x in myresult:
+        
+        if(email==x[0]):
             return (False,"El E-mail ya esta registrado")
     mycursor = connection.cursor()
-    print("si llegue")
+ 
     mycursor.execute('''INSERT INTO `loveinbox`.`usuario` (`E_MAIL`, `NOMBRE`, `CONTRASENA`, `DIRECCION`, `TARJETA`, `PUNTOS`) 
         VALUES ('%s', '%s', '%s', '%s', '%s', '0')'''%(email, nombre,contrasena,direccion,tarjeta))
     connection.commit()
     return (True,"Usuario registrado correctamente")
 
-def login(connection, email,contrasena):
+def loginQuery(connection, email,contrasena):
     mycursor = connection.cursor()
     mycursor.execute("SELECT `E_MAIL` FROM `loveinbox`.`usuario`;")
 
     myresult = mycursor.fetchall()
 
-    print(myresult)
+    for x in myresult:
+        if(email==x[0]):
+            
+            mycursor = connection.cursor()
+            mycursor.execute("SELECT `CONTRASENA` FROM `loveinbox`.`usuario` WHERE `E_MAIL` = '"+ email + "';")
 
-def InfoEmpresa(connection, nit):
+            myresult2 = mycursor.fetchall()
+
+            for y in myresult2:
+                if(contrasena==y[0]):
+                    return True
+    return False 
+
+def InfoEmpresaQuery(connection, nit):
     mycursor = connection.cursor()
     mycursor.execute("SELECT * FROM `loveinbox`.`empresa` WHERE `NIT` = `"+ nit + "`;")
     myresult = mycursor.fetchall()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,33 @@
 from multiprocessing.connection import Connection
-from flask import Flask
+from flask import Flask,jsonify
 #from datetime import datetime
 #from flask_cors import CORS
 import BD.bdConnection as bdConnection
 import BD.query as query
 
 connection=bdConnection.bdConnection()
+#print(query.register(connection,'cgalvis@unal.edu.co','abc','1234567','cr2','4536456'))
 
 app = Flask(__name__)
 
+@app.route('/',methods=['GET'])
+def index():
+    return jsonify({'LoveInBox esta funcionando!': True})
+
+@app.route('/register/<email>/<nombre>/<contrasena>/<direccion>/<tarjeta>', methods = ['POST'])
+def register(email, nombre,contrasena,direccion,tarjeta):
+    ret=query.registerQuery(connection, email, nombre,contrasena,direccion,tarjeta)
+    return jsonify({'Estado': ret[0],
+    'Detalles': ret[1]})
+
+@app.route('/login/<email>/<contrasena>', methods = ['POST'])
+def login(email,contrasena):
+    ret=query.loginQuery(connection, email,contrasena)
+    return jsonify({'Login': ret})
+
+#@app.route('/infoEmpresa/<email>/<contrasena>', methods = ['POST'])
+
 if __name__=="__main__":
-    app.run(debug=True) #como npm start
-    #print(query.login(connection,'asado','c'))
+    app.run(debug=True) #como npm startgit 
+    
 
