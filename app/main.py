d5a9f9f67cdcb9a35379f75c7b406206fdf35c17
Juan Bejarano <jbejaranos@unal.edu.co>
2022-11-21 20:15:49 -0500

    port environ added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from multiprocessing.connection import Connection
 from flask import Flask,jsonify
 #from datetime import datetime
 #from flask_cors import CORS
+import os
 import BD.bdConnection as bdConnection
 import BD.query as query
 
@@ -40,6 +41,6 @@ def loginAdmn(email,contrasena):
     return jsonify({'Login': ret})
 
 if __name__=="__main__":
-    app.run(debug=True,host='0.0.0.0',port=5000) #como npm startgit 
+    app.run(debug=True,host='0.0.0.0',port=os.environ.get("PORT", 5000)) #como npm startgit 
     
 
